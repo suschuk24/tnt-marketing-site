077c0fbad52d7589799c169b3ea3e367ebf3d69e
Seth Uschuk <seth@hypercolordigital.com>
2022-04-27 14:25:30 -0500

    added basic content for gg page, needs styling

---
+++ src/containers/DaycareCenters/GoldenGateway/components/About/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from '../../../../../tnt-gg-logo.png'
+
+type Props = {
+    name: string
+    description: string
+    tagLine: string
+    address: string
+    address1?: string
+
+    hours: string
+    phone: string
+}
+const About: React.FC<Props> = ({name, description, tagLine, address, address1, hours, phone}) => {
+    return (
+        <div>
+            <img src={logo} />
+            <div>
+                <h3>{phone}</h3>
+                <p>{address}</p>
+                <p>{address1}</p>
+            </div>
+            <h2>{name}</h2>
+            <div>
+                <h4>{tagLine}</h4>
+                <p>{description}</p>
+            </div>
+        </div>
+
+    )
+}
+export default About
--- src/containers/DaycareCenters/GoldenGateway/index.tsx
+++ src/containers/DaycareCenters/GoldenGateway/index.tsx
@@ -1,46 +1,44 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import './App.css';
-import Header from "../../components/Header";
-import HomeScreen from '../HomeScreen';
-import Footer from "../../components/Footer"
-import GoldenGateway from "../DaycareCenters/GoldenGateway";
+import About from "./components/About";
+import React from "react";
 
-const App = () => {
+type Props = {
+    name: string
+    address: string
+    address1: string
+    hours: string
+    phone: string
+}
+const GoldenGateway: React.FC<Props> = ({name, address, address1, hours, phone}) => {
     const goldenGatewayData = {
-        name: 'Truks-N-Truks Golden Gateway',
+        name,
         description: 'At Truks-N-Tykes @ Golden Gateway, our hands-on educational method helps children learn through play. We balance structured activities with free play, which helps kids thrive. Our center offers care for a wide range of children — from infants all the way to school-age — through all-day preschool and Pre-K, and Junior-K led by a thoughtful, educated staff. Junior-K is specifically designed to help kids develop skills for success in kindergarten, but it still prioritizes play as the ‘hard work’ of childhood. Students explore letter recognition, phonetics, fine motor activities and math/science each day. Our goal is for each student to enter kindergarten with confidence in both academic and social skills. Once in school, we offer a before/after school (summer) care program that will allow your school-aged child more gym, art, music and outdoor time along with field trips. Truks-N-Trykes @ Golden Gateway is invested in all ages and stages of your child’s development, providing them with resources and skills for success. With a wide range of offerings and a highly trained staff, Truks-N-Tykes @ Golden Gateway is designed to meet all your early childhood education needs.',
         tagLine: 'Child Care Programs for Ages –\n' +
             'Infants to 12 years old',
-        address: '5700 E Tahoe Street',
-        address1: 'Sioux Falls, SD 57110',
-        phoneNumber: '(605) 271-3668',
-        emailAddress: 'tntgoldengateway@gmail.com',
-        hours: '6:30am-6:00pm M-F',
-        phone: '605-271-3668'
+        address,
+        address1,
+        hours,
+        phone
     }
-  return (
-    <Router>
-      <Header />
-      <main className="App">
-          <Routes>
-            <Route path='/' element={
-                <HomeScreen />
-            }/>
-            <Route path = '/golden-gateway' element={
-                <GoldenGateway
-                    name={goldenGatewayData.name}
-                    address={goldenGatewayData.address}
-                    address1={goldenGatewayData.address1}
-                    hours = {goldenGatewayData.hours}
-                    phone={goldenGatewayData.phone}
-                />
-            }/>
-          </Routes>
-      </main>
-      <Footer />
-    </Router>
-  );
+
+    return (
+        <>
+            <About
+                name={goldenGatewayData.name}
+                description={goldenGatewayData.description}
+                tagLine={goldenGatewayData.tagLine}
+                address={goldenGatewayData.address}
+                address1={goldenGatewayData.address1}
+                hours={goldenGatewayData.hours}
+                phone={goldenGatewayData.phone}
+            />
+            <div>
+                <button />
+                <button />
+                <button />
+                <button />
+            </div>
+        </>
+    )
 }
 
-export default App;
+export default GoldenGateway
